9b4eabeaa51ae42fd53ebe57837618b92fa5951d
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2024-11-06 21:49:08 +0600

    ui: table view in modal

--- src/components/filePicker.js
+++ src/components/filePicker.js
@@ -1,27 +1,41 @@
 // FilePickerModal.js
-import React from 'react';
+import React from "react";
 
 function FilePickerModal({ isOpen, onClose, files }) {
-  console.log( files)
+  console.log(files);
 
   if (!isOpen) return null; // Don't render if the modal is closed
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="flex flex-col bg-white p-6 rounded shadow-lg w-80 items-center justify-center">
-      <h2 className="text-lg font-bold mb-4">Uploaded Files</h2>
+      <div className="flex flex-col bg-white p-6 rounded shadow-lg max-w-[45%] items-center justify-center">
+        <h2 className="text-lg font-bold mb-4">Uploaded Files</h2>
         {/* Display selected file names and extensions */}
         {files.length > 0 && (
           <div className="mt-4">
-            <ul className="list-disc pl-5">
-              {files.map((file, index) => (
-                <li key={index}>
-                  {file.name}
-                </li>
-              ))}
-            </ul>
+            <table className="min-w-full table-auto">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2 border">File Name</th>
+                  <th className="px-4 py-2 border">Extension</th>
+                </tr>
+              </thead>
+              <tbody>
+                {files.map((file, index) => (
+                  <tr key={index}>
+                    <td className="px-4 py-2 border">
+                      {file.name.split(".")[0]}
+                    </td>
+                    <td className="px-4 py-2 border">
+                      {file.name.split(".")[1]}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         )}
+
         <div className="mt-4">
           <button
             className="px-4 py-2 mr-2 bg-red-500 text-white rounded hover:bg-red-600"
@@ -29,7 +43,7 @@ function FilePickerModal({ isOpen, onClose, files }) {
           >
             Close
           </button>
-          </div>
+        </div>
       </div>
     </div>
   );
